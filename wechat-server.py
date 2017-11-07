207b76b03b319620c66a14cf0187b0484e6cac0d
zhengxin <zhengxin@kyee.com.cn>
2017-11-07 10:59:42 +0800

    bug fix

--- wechat-server.py
+++ wechat-server.py
@@ -6,17 +6,15 @@ from msgCrypt.WXBizMsgCrypt import WXBizMsgCrypt
 from msgCrypt.settings import msg_crypt
 
 class MainHandler(tornado.web.RequestHandler):
-    acccess_token=''
     def get(self):
-        global msgDecode
         msg_signature = self.get_argument("msg_signature", "")
         timestamp = self.get_argument("timestamp", "")
         nonce = self.get_argument("nonce", "")
         echostr = self.get_argument("echostr", "")
-        ret,replyEchoStr = msgDecode.VerifyURL(sMsgSignature=msg_signature,sTimeStamp=timestamp,sNonce=nonce,sEchoStr=echostr)
+        ret,replyEchoStr = msg_crypt.VerifyURL(sMsgSignature=msg_signature,sTimeStamp=timestamp,sNonce=nonce,sEchoStr=echostr)
         self.write(replyEchoStr)
     def post(self):
-        global msgDecode
+        pass
 
 
 application = tornado.web.Application([
@@ -24,11 +22,10 @@ application = tornado.web.Application([
 ])
 
 def test():
-    global msgDecode
-    print msgDecode.UpdateAccessToken()
+    print msg_crypt.UpdateAccessToken()
 
 
 if __name__=="__main__":
-    # application.listen(8002)
-    # tornado.ioloop.IOLoop.instance().start()
-    test()
+    application.listen(8002)
+    tornado.ioloop.IOLoop.instance().start()
+    # test()
