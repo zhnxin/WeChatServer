ef3088d3cf936407a5a41f4b4b8b5b64a00b9763
zhengxin <zhngxin@aliyun.com>
2018-07-03 11:16:29 +0800

    mofidy latest wxbizmsgcrypt

---
+++ msgCrypt/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+from .WXBizzMsgCrypt import WXBizMsgCrypt
+from .settings import CONFIG,logger
+
+def loadMsgCryptMap():
+    MsgCryptMap={}
+    CorpID = CONFIG['wechat']['CorpID']
+    for app,args in CONFIG['wechat']['app'].items():
+        MsgCryptMap[app] = WXBizMsgCrypt(sToken=args['Token'],
+         sEncodingAESKey=args['EncodingAESKey'],
+         sCorpId=CorpID,sAgentId=args['AgentId'], sCorpsecret=args['Secret'])
+    return MsgCryptMap
+
+MSGCRYPTMAP = loadMsgCryptMap()
